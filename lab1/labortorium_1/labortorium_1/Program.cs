98cee9c57e0c40a63b4127fbbf9f47fcaad603d5
przemak19 <przemak19@gmail.com>
2025-03-08 20:51:39 +0100

    Trzecie zadanie działa ale nie wiem czy dobre wyniki testów wychodzą

--- lab1/labortorium_1/labortorium_1/Program.cs
+++ lab1/labortorium_1/labortorium_1/Program.cs
@@ -8,9 +8,9 @@ class Program
     static void Main(string[] args)
     {
         string inputFile = "", outputFile = "", keyFile = "";
-        string gramFile = "";
-        bool encrypt = false, decrypt = false;
-        int nGram = 0;
+        string gramFile = "", refFile = "";
+        bool encrypt = false, decrypt = false, calculateChiSquare = false;
+        int nGram = 0, refNGram = 0;
 
         for (int i = 0; i < args.Length; i++)
         {
@@ -21,6 +21,7 @@ class Program
                 case "-k": keyFile = args[++i]; break;
                 case "-e": encrypt = true; break;
                 case "-d": decrypt = true; break;
+                case "-s": calculateChiSquare = true; break;
                 case "-g1":
                 case "-g2":
                 case "-g3":
@@ -28,12 +29,19 @@ class Program
                     nGram = int.Parse(args[i].Substring(2, 1));
                     gramFile = args[++i];
                     break;
+                case "-r1":
+                case "-r2":
+                case "-r3":
+                case "-r4":
+                    refNGram = int.Parse(args[i].Substring(2, 1));
+                    refFile = args[++i];
+                    break;
             }
         }
 
-        if ((encrypt == decrypt) && nGram == 0 || string.IsNullOrEmpty(inputFile))
+        if ((encrypt == decrypt) && nGram == 0 && !calculateChiSquare || string.IsNullOrEmpty(inputFile))
         {
-            Console.WriteLine("Użycie: program -e|-d -k klucz.txt -i wejscie.txt -o wyjscie.txt [-g1|-g2|-g3|-g4 gram.txt]");
+            Console.WriteLine("Użycie: program -e|-d -k klucz.txt -i wejscie.txt -o wyjscie.txt [-g1|-g2|-g3|-g4 gram.txt] [-rX baza.txt] [-s]");
             return;
         }
 
@@ -53,6 +61,14 @@ class Program
             var nGramCounts = GenerateNGrams(inputText, nGram);
             SaveNGrams(gramFile, nGramCounts);
             Console.WriteLine($"Statystyki {nGram}-gramów zapisano do {gramFile}");
+        }
+
+        if (calculateChiSquare && !string.IsNullOrEmpty(refFile) && refNGram > 0)
+        {
+            var observed = GenerateNGrams(inputText, refNGram);
+            var expected = LoadReferenceNGrams(refFile);
+            double chiSquare = CalculateChiSquare(observed, expected);
+            Console.WriteLine($"Wartość testu chi-kwadrat: {chiSquare:F4}");
         }
     }
 
@@ -114,5 +130,39 @@ class Program
                 writer.WriteLine($"{kvp.Key}	{kvp.Value}");
             }
         }
+    }
+
+    static Dictionary<string, double> LoadReferenceNGrams(string filename)
+    {
+        var reference = new Dictionary<string, double>();
+        double total = 0;
+
+        foreach (var line in File.ReadLines(filename))
+        {
+            var parts = line.Split();
+            if (parts.Length == 2 && double.TryParse(parts[1], out double probability))
+            {
+                reference[parts[0]] = probability;
+                total += probability;
+            }
+        }
+
+        return reference;
+    }
+
+    static double CalculateChiSquare(Dictionary<string, int> observed, Dictionary<string, double> expected)
+    {
+        double chiSquare = 0.0;
+        int totalObserved = observed.Values.Sum();
+
+        foreach (var kvp in expected)
+        {
+            int observedCount = observed.ContainsKey(kvp.Key) ? observed[kvp.Key] : 0;
+            double expectedCount = totalObserved * kvp.Value;
+
+            chiSquare += Math.Pow(observedCount - expectedCount, 2) / expectedCount;
+        }
+
+        return chiSquare;
     }
 }
--- lab1/labortorium_1/labortorium_1/obj/Debug/net8.0/labortorium_1.AssemblyInfo.cs
+++ lab1/labortorium_1/labortorium_1/obj/Debug/net8.0/labortorium_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("labortorium_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3a0c62265f2180a99a9ffc24c65747d7dddb93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccf726943e44988b6cf7a30371ad627124523f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("labortorium_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("labortorium_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
